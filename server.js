bc284448d0570c7acc380e019b3be30340bc4c10
hanh <hanh@gmail.com>
2023-10-25 19:04:50 +0700

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'name',
   streamUrl: 'rtsp://admin:admin1234@192.168.1.15:554/cam/realmonitor?channel=2&subtype=0',
-  wsPort: 9999,
+  wsPort: process.env.PORT || 9999,
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
     '-r': 30 // options with required values specify the value after the key
