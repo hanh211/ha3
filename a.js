4ee32f67a15a573a7d09edc65ccfb50da5482486
hanh <hanh@gmail.com>
2023-12-21 05:42:41 +0700

    a

--- a.js
+++ a.js
@@ -5,7 +5,8 @@ const server = require("http").Server(app);
 app.set("view engine","ejs");
 app.set("views","public");
 // const port=3000;
-app.listen(process.env.PORT || 3000);
+// app.listen(process.env.PORT || 3000);
+app.listen("https://hanh211.github.io/ha3");
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:false}));
 
